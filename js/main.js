074e88557dad6302c4e3d2cd18d1be557e79297a
Ekaterina <Kata_Yurevna@mail.ru>
2020-06-17 02:00:17 +0700

    mainPin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var COUNT = 8;
+var INTO = 'Enter';
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 
@@ -34,7 +35,7 @@ var shuffleArray = function (a) {
 };
 
 var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+// map.classList.remove('map--faded');
 
 var addAdverts = function () {
   for (var i = 0; i < COUNT; i++) {
@@ -89,65 +90,94 @@ var renderPins = function () {
   }
   similarListElement.appendChild(fragment);
 };
-renderPins();
+
 
 // Ветка3-задача3
 
-var cardTemplate = document.querySelector('#card')
-    .content
-    .querySelector('.map__card');
+// var cardTemplate = document.querySelector('#card')
+//     .content
+//     .querySelector('.map__card');
 
-var renderCard = function (card) {
-  var cardElement = cardTemplate.cloneNode(true);
+// var renderCard = function (card) {
+//   var cardElement = cardTemplate.cloneNode(true);
 
-  var popupTitle = cardElement.querySelector('.popup__title');
-  popupTitle.textContent = card.offer.title;
+//   var popupTitle = cardElement.querySelector('.popup__title');
+//   popupTitle.textContent = card.offer.title;
 
-  var popupTextAddress = cardElement.querySelector('.popup__text--address');
-  popupTextAddress.textContent = card.offer.address;
+//   var popupTextAddress = cardElement.querySelector('.popup__text--address');
+//   popupTextAddress.textContent = card.offer.address;
 
-  var popupTextPrice = cardElement.querySelector('.popup__text--price');
-  popupTextPrice.textContent = card.offer.price + '₽/ночь';
+//   var popupTextPrice = cardElement.querySelector('.popup__text--price');
+//   popupTextPrice.textContent = card.offer.price + '₽/ночь';
 
-  var popupType = cardElement.querySelector('.popup__type');
-  popupType.textContent = typeTranslate[card.offer.type].ru;
+//   var popupType = cardElement.querySelector('.popup__type');
+//   popupType.textContent = typeTranslate[card.offer.type].ru;
 
-  var popupTextCapacity = cardElement.querySelector('.popup__text--capacity');
-  popupTextCapacity.textContent = card.offer.rooms + 'комнаты для' + card.offer.guests + 'гостей';
+//   var popupTextCapacity = cardElement.querySelector('.popup__text--capacity');
+//   popupTextCapacity.textContent = card.offer.rooms + 'комнаты для' + card.offer.guests + 'гостей';
 
-  var popupTextTime = cardElement.querySelector('.popup__text--time');
-  popupTextTime.textContent = 'заезд после' + card.offer.checkin + 'выезд до' + card.offer.checkout;
+//   var popupTextTime = cardElement.querySelector('.popup__text--time');
+//   popupTextTime.textContent = 'заезд после' + card.offer.checkin + 'выезд до' + card.offer.checkout;
 
-  var popupFeatures = cardElement.querySelector('.popup__features');
-  var featureNodes = popupFeatures.querySelectorAll('.popup__feature');
-  for (var i = 0; i < featureNodes.length; i++) {
-    if (card.offer.features.indexOf(featureNodes[i].classList[1].replace('popup__feature--', '')) >= 0) {
-      featureNodes[i].remove();
-    }
+//   var popupFeatures = cardElement.querySelector('.popup__features');
+//   var featureNodes = popupFeatures.querySelectorAll('.popup__feature');
+//   for (var i = 0; i < featureNodes.length; i++) {
+//     if (card.offer.features.indexOf(featureNodes[i].classList[1].replace('popup__feature--', '')) >= 0) {
+//       featureNodes[i].remove();
+//     }
+//   }
+
+//   var popupDescription = cardElement.querySelector('.popup__description');
+//   popupDescription.textContent = card.offer.description;
+
+//   var popupPhotos = cardElement.querySelector('.popup__photos');
+//   var photo = popupPhotos.querySelector('img');
+//   if (card.offer.photos.length > 0) {
+//     photo.src = card.offer.photos[0];
+//     for (i = 1; i < card.offer.photos.length; i++) {
+//       var photoElement = photo.cloneNode(true);
+//       photoElement.src = card.offer.photos[i];
+//       popupPhotos.appendChild(photoElement);
+//     }
+//   } else {
+//     photo.remove();
+//   }
+//   popupPhotos.src = card.offer.photos.length;
+
+//   var popupAvatar = cardElement.querySelector('.popup__avatar');
+//   popupAvatar.src = card.author.avatar;
+
+//   return cardElement;
+// };
+
+// var mapFilters = map.querySelector('.map__filters-container');
+// map.insertBefore(renderCard(adverts[0]), mapFilters);
+
+var form = document.querySelector('.ad-form');
+var formFields = document.querySelectorAll('fieldset, .map__filter');
+
+var setFormState = function () {
+  for (var i = 0; i < formFields.length; i++) {
+    formFields[i].disabled = !formFields[i].disabled;
   }
+};
+setFormState();
 
-  var popupDescription = cardElement.querySelector('.popup__description');
-  popupDescription.textContent = card.offer.description;
+var mainPin = document.querySelector('.map__pin--main');
 
-  var popupPhotos = cardElement.querySelector('.popup__photos');
-  var photo = popupPhotos.querySelector('img');
-  if (card.offer.photos.length > 0) {
-    photo.src = card.offer.photos[0];
-    for (i = 1; i < card.offer.photos.length; i++) {
-      var photoElement = photo.cloneNode(true);
-      photoElement.src = card.offer.photos[i];
-      popupPhotos.appendChild(photoElement);
-    }
-  } else {
-    photo.remove();
+var onMainPinMouseDown = function (evt) {
+  if (evt.button === 0) {
+    map.classList.remove('map--faded'); // уже была эта строчка
+    setFormState();
+    renderPins();
+    mainPin.removeEventListener('mousedown', onMainPinMouseDown);
   }
-  popupPhotos.src = card.offer.photos.length;
-
-  var popupAvatar = cardElement.querySelector('.popup__avatar');
-  popupAvatar.src = card.author.avatar;
-
-  return cardElement;
 };
 
-var mapFilters = map.querySelector('.map__filters-container');
-map.insertBefore(renderCard(adverts[0]), mapFilters);
+mainPin.addEventListener('mousedown', onMainPinMouseDown);
+
+mainPin.addEventListener('keydown', function (evt) {
+  if (evt.key === INTO) {
+    map.classList.remove('map--faded'); // уже была эта строчка, загнать в переменную
+  }
+});
